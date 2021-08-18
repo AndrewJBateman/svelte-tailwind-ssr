3b7a5066372e3b58bf82b06175288c67edb255cb
AndrewJBateman <gomezbateman@yahoo.com>
2021-08-18 20:59:12 +0200

    feat: about page + nav added

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,4 +1,43 @@
-<nav class="flex justify-left w-full p-2 mb-4 text-white bg-blue-900">
-  <a class="mx-4 text-lg" href="/">Home</a>
-  <a class="mx-4 text-lg" href="/about">About</a>
+<script>
+  // core components
+  const globeLoc = "../assets/globe.jpg";
+</script>
+
+<nav
+  class="flex flex-row content-center justify-between w-full font-sans text-center text-white bg-blue-900 shadow sm:items-baseline"
+>
+  <!--navbar LHS-->
+  <div class="self-center">
+    <a
+      class="mx-2 bg-blue-900"
+      aria-label="home nav link"
+      href="/"
+    >
+      <div class="flex mx-2">
+        <img
+          class="mx-2 hover:bg-blue-400"
+          src="{globeLoc}"
+          width="30" height="30"
+          alt="Globe"
+        />
+        <p class="pt-1 hover:bg-blue-400">Countries</p>
+      </div>
+    </a>
+  </div>
+
+  <!--navbar RHS-->
+  <div class="self-center">
+    <a
+      class="bg-blue-900 mx-4 rounded hover:bg-blue-400"
+      aria-label="about nav link"
+      href="/about"
+      >About</a
+    >
+    <a
+      class="bg-blue-900 mx-4 rounded hover:bg-blue-400"
+      aria-label="contact nav link"
+      href="/contact"
+      >Contact</a
+    >
+  </div>
 </nav>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,4 +1,79 @@
 <svelte:head>
-  <title>About Svelte Kit API Data</title>
+  <title>About Svelte Countries API App</title>
 </svelte:head>
-<h1 classs="text-4xl text-center my-8 uppercase">About App</h1>
+<div class="min-h-screen min-w-screen">
+  <div class="flex justify-center w-full h-screen pt-10 mx-auto">
+    <div class="max-w-xs">
+      <div class="rounded-lg bg-gray-100 shadow-lg">
+        <div class="p-2 pt-5 pb-0 photo-wrapper">
+          <img
+            class="w-40 h-40 mx-auto rounded-full"
+            width="40px"
+            height="40px"
+            src="../../../assets/globe-large.png"
+            alt="globe"
+          />
+        </div>
+
+        <div class="p-2">
+          <h4
+            class="pb-1 text-xl font-medium leading-8 text-center text-gray-900"
+          >
+          Countries API
+          </h4>
+          <div class="px-2 text-xs font-semibold text-left text-gray-600">
+            <p class="pb-2">
+              <span class="font-bold text-gray-900"
+                >A list of countries is fetched from the REST Countries API</span
+              >
+              using a simple http fetch and javascript map method. The country Alpha3 Code is used as the id so user can navigate to a Country Detail page.
+            </p>
+            <p>
+              <span class="font-bold text-gray-900"
+                >The data on the Contact page</span
+              >
+              is fetched from the Github API. No API key is required to search
+              for a user.
+            </p>
+          </div>
+          <table class="my-2 text-xs">
+            <tbody>
+              <tr>
+                <td class="px-2 font-semibold text-gray-500">
+                  <span class="font-bold text-gray-900">The Stack:</span>
+                </td>
+                <td class="px-2 text-xs text-gray-600">
+                  Svelte, Tailwindcss
+                </td>
+              </tr>
+              <tr>
+                <td class="px-2">
+                  <span class="font-bold text-gray-900">Testing:</span>
+                </td>
+                <td class="px-2 text-xs text-gray-600">tba</td>
+              </tr>
+              <tr>
+                <td class="px-2">
+                  <span class="font-bold text-gray-900">Image Credit:</span>
+                </td>
+                <td class="px-2">
+                  <td class="px-2 text-xs text-gray-600">tba
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div class="my-2 text-center">
+            <a
+              class="text-xs italic font-medium text-indigo-600 hover:underline hover:text-indigo-800"
+              href="https://github.com/AndrewJBateman/svelte-tailwind-ssr"
+              target="_blank"
+              rel="noopener noreferrer"
+              >See Github Repo.</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
