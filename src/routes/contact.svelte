e88870134c11424546c22a970c227cc0189e6138
AndrewJBateman <gomezbateman@yahoo.com>
2021-08-20 18:18:30 +0200

    feat: add user info. card

---
+++ src/routes/contact.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	import { user, derivedDate } from './../stores/datastore';
+	import UserCard from './../components/userCard.svelte';
+</script>
+
+<svelte:head>
+	<title>Contact Page</title>
+</svelte:head>
+
+<main>
+	<div>
+		<UserCard {user} {derivedDate} />
+	</div>
+</main>
--- src/stores/datastore.js
+++ src/stores/datastore.js
@@ -1,32 +1,50 @@
 import { writable } from 'svelte/store';
+import { startWith, map } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 
 export const countries = writable([]);
+export const derivedDate = new Date().toLocaleDateString("en-GB");
 const countryDetails = {};
 
+// Fetch user data from Github REST API
+const baseUrl = 'https://api.github.com/users/';
+const userSearchUrl = `${baseUrl + 'AndrewJBateman'}`;
+const token = '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';
+export const user = ajax({
+	url: userSearchUrl,
+	headers: {
+		authorization: `token ${token}`
+	}
+}).pipe(
+	map((x) => x.response),
+	startWith([])
+);
+
+// Fetch JSON data - 3 fields only - for all countries from a REST API
 export const fetchCountries = async () => {
 	const url = 'https://restcountries.eu/rest/v2/all?fields=name;flag;alpha3Code';
 	const res = await fetch(url);
 	const data = await res.json();
 	const loadedData = data.map((data) => ({
 		name: data.name,
-    id: data.alpha3Code.toLowerCase(),
-		image: data.flag,
+		id: data.alpha3Code.toLowerCase(),
+		image: data.flag
 	}));
 	countries.set(loadedData);
 };
 
+// Fetch JSON data on country with alpha3 code matching id from a REST API
 export const fetchCountryById = async (id) => {
-  if (countryDetails[id]) return countryDetails[id];
+	if (countryDetails[id]) return countryDetails[id];
 
-  try {
-    const url = `https://restcountries.eu/rest/v2/alpha/${id}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    countryDetails[id] = data;
-    return data;
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
-
-}
+	try {
+		const url = `https://restcountries.eu/rest/v2/alpha/${id}`;
+		const res = await fetch(url);
+		const data = await res.json();
+		countryDetails[id] = data;
+		return data;
+	} catch (err) {
+		console.error(err);
+		return null;
+	}
+};
